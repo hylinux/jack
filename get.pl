a60c74567867e364b4f7dd0a2a682032312f8d34
Mike.G <ghw@x200.5anet.com>
2012-07-08 15:49:03 +0800

    add css support

--- get.pl
+++ get.pl
@@ -22,36 +22,61 @@ sub start {
     my($self, $tag, $attr, $attrseq, $origtext) = @_;
 
     #处理图片的
-    if ($tag =~ /^img$/ ) {
-        if ( defined $attr->{'src'} ) {
-            my $img_src = $attr->{'src'};
-            my $img_url = '';
-            if ( $img_src =~ /^http/ ) {
-                $img_url = $img_src;
+#    if ($tag =~ /^img$/ ) {
+#        if ( defined $attr->{'src'} ) {
+#            my $img_src = $attr->{'src'};
+#            my $img_url = '';
+#            if ( $img_src =~ /^http/ ) {
+#                $img_url = $img_src;
+#            } else {
+#                $img_url = $original_url.$img_src;
+#            }
+#            print "Image Url is:", $img_url, "\n";
+#
+#            #取得文件的名字
+#            my $file_ext = "";
+#            if ( $img_src =~ /\.jpg/gi || $img_src =~ /\.jpeg/gi ) {
+#                 $file_ext = 'jpg';
+#            } elsif ( $img_src =~ /\.gif/gi ) {
+#                 $file_ext = 'gif';
+#            } elsif ( $img_src =~ /\.png/gi ) {
+#                 $file_ext = 'png';
+#            } else {
+#                $file_ext = 'jpg';
+#            }
+#
+#            my $file_name = int(rand(300)).'.'.$file_ext;
+#            print "File name is:", $file_name, "\n";
+#            
+#            getstore($img_url, $file_name);
+#            $replace_hash_img{$img_src} = $file_name;
+#        }
+#    }
+#
+    #开始处理css和javascript
+
+    if ( $tag =~ /^link$/ ) {
+
+        if (defined $attr->{'type'} && $attr->{'type'} =~ /text\/css/ ) {
+            #CSS
+            
+            #取得CSS的位置
+            my $css_src = $attr->{'href'};
+            my $css_url = ''; 
+            if ($css_src =~ /^http/i ) {
+                $css_url = $css_src;
             } else {
-                $img_url = $original_url.$img_src;
-            }
-            print "Image Url is:", $img_url, "\n";
-
-            #取得文件的名字
-            my $file_ext = "";
-            if ( $img_src =~ /\.jpg/gi || $img_src =~ /\.jpeg/gi ) {
-                 $file_ext = 'jpg';
-            } elsif ( $img_src =~ /\.gif/gi ) {
-                 $file_ext = 'gif';
-            } elsif ( $img_src =~ /\.png/gi ) {
-                 $file_ext = 'png';
-            } else {
-                $file_ext = 'jpg';
+                $css_url = $original_url.$css_src;
             }
 
-
-            my $file_name = int(rand(300)).'.'.$file_ext;
+            print "CSS url:", $css_url, "\n";
+            my $file_name = int(rand(300)).'.css';
             print "File name is:", $file_name, "\n";
             
-            getstore($img_url, $file_name);
-            $replace_hash_img{$img_src} = $file_name;
-        }
+            getstore($css_url, $file_name);
+            $replace_hash_css{$css_src} = $file_name;
+
+        } 
     }
 }
 
